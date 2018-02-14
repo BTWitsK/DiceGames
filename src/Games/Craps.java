977a05ad6f5c28ac9a5ea2d1a4761b09ba76610f
BTWitsK <BTWitsK@Gmail.com>
2018-02-14 15:27:43 -0600

    set todo's for craps class

--- src/Games/Craps.java
+++ src/Games/Craps.java
@@ -2,5 +2,28 @@ package Games;
 import Games.Dice.SixSiders;
 
 public class Craps {
-    //TODO: Implement Craps game
+    private boolean passline;
+
+
+    public boolean isPassline() {
+        return passline;
+    }
+
+    public void setPassline(){
+        //TODO: set passline function
+    }
+
+    public int comeOut(){
+        //TODO: implement comeout
+        return -1;
+    }
+
+    public int postBet(){
+        //TODO: implement postbet
+        return -1;
+    }
+
+    public void bet(){
+        //TODO: implement bet
+    }
 }
